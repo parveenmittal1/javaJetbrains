aa3b923c495b8aa7355e4d0b46dca18a16b5aa59
parveen <mittalparveen652@gmail.com>
2020-03-08 13:37:32 +0530

    input

---
+++ InputScanning/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        // put your code here
+        Scanner s=new Scanner(System.in);
+        String c1 =s.next();
+        String c2 =s.next();
+        String c3 =s.next();
+        String c4 =s.next();
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(c3);
+        System.out.println(c4);
+    }
+}
