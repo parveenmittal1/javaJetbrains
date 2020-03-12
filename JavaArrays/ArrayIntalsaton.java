edc14e6b67550732971b7d38d7f634865323d3cd
parveen <mittalparveen652@gmail.com>
2020-03-12 13:04:33 +0530

    dfsfdd
    rgdrg

---
+++ JavaArrays/ArrayIntalsaton.java
@@ -0,0 +1,9 @@
+char[] array = new char[10000000000000];
+
+        char[] array1 = new char[-1];
+
+        char[] array2 = { 'a', 'b', 'c', 'd' };
+
+        char[] array3 = new char[0];
+
+        char[] array4 = new char[1];
--- gitProject/Robot.java
+++ gitProject/Robot.java
@@ -58,7 +58,15 @@ public class SimpleBot {
         }else {
             System.out.println("Please, try again.");
         }
+        char[] array = new char[10000000000000];
 
+        char[] array1 = new char[-1];
+
+        char[] array2 = { 'a', 'b', 'c', 'd' };
+
+        char[] array3 = new char[0];
+
+        char[] array4 = new char[1];
     }
 
     static void end() {
